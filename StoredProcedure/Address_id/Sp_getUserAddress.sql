cf839ce2f6b800edc4f1b50a276d38afae3ccb00
rani1029 <ranig1029@gmail.com>
2022-01-18 03:03:15 +0530

    [rani] get addresses stored procedure added

---
+++ StoredProcedure/Address_id/Sp_getUserAddress.sql
@@ -0,0 +1,37 @@
+Create PROCEDURE Sp_GetAddressesOfUser
+  @UserId int
+AS
+BEGIN
+ BEGIN TRY 
+    BEGIN TRANSACTION;
+
+	IF(EXISTS(SELECT * FROM Address WHERE UserId=@UserId))
+	 begin
+	   SELECT * FROM Address WHERE UserId=@UserId;
+   	 end
+	 else
+	 begin
+		select 1
+	end
+       
+    COMMIT TRANSACTION;
+ END TRY
+ BEGIN CATCH
+  IF (XACT_STATE()) = -1  
+		BEGIN  
+			PRINT 'The transaction is in an uncommittable state.' +  
+					'Rolling back transaction.'  
+			ROLLBACK TRANSACTION;  
+		END;  
+        
+		-- Test if the transaction is committable.  
+		IF (XACT_STATE()) = 1  
+		BEGIN  
+			PRINT 'The transaction is committable.' +  
+				'Committing transaction.'  
+			COMMIT TRANSACTION;     
+		END;  
+  END CATCH
+  END 
+
+     
